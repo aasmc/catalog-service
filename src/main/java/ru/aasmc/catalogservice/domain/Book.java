32fdd5ffab2e69435854041ed8d2ee0d4368beff
aleksas <aasmc@yandex.ru>
2023-03-16 16:07:32 +0300

    test: add tests for bookValidation and catalogServiceApplication

--- src/main/java/ru/aasmc/catalogservice/domain/Book.java
+++ src/main/java/ru/aasmc/catalogservice/domain/Book.java
@@ -10,7 +10,7 @@ public record Book(
         @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
         String isbn,
 
-        @NotBlank(message = "The book title must be defined")
+        @NotBlank(message = "The book title must be defined.")
         String title,
 
         @NotBlank(message = "The book author must be defined.")
