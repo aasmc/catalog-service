29b1a716f08e8f91b479fd4e0e9ab32e2bcbd1f2
aleksas <aasmc@yandex.ru>
2023-03-17 16:43:08 +0300

    add configuration properties for home controller

---
+++ src/main/java/ru/aasmc/catalogservice/web/HomeController.java
@@ -0,0 +1,20 @@
+package ru.aasmc.catalogservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.aasmc.catalogservice.config.PolarProperties;
+
+@RestController
+public class HomeController {
+
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return polarProperties.getGreeting();
+    }
+}
