c0154e7bbef69f3bb72dd166fe9b60b91e55a243
aleksas <aasmc@yandex.ru>
2023-03-21 14:16:59 +0300

    feat: implement persistence with Spring Data JDBC, Postgres, Flyway, TestContainers

--- src/main/java/ru/aasmc/catalogservice/domain/Book.java
+++ src/main/java/ru/aasmc/catalogservice/domain/Book.java
@@ -1,11 +1,19 @@
 package ru.aasmc.catalogservice.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
+import java.time.Instant;
 
 public record Book(
+        @Id
+        Long id,
         @NotBlank(message = "The book ISBN must be defined.")
         @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
         String isbn,
@@ -18,6 +26,25 @@ public record Book(
 
         @NotNull(message = "The book price must be defined.")
         @Positive(message = "The book price must be greater than zero.")
-        Double price
+        Double price,
+
+        String publisher,
+
+        @CreatedDate
+        Instant createdDate,
+
+        @LastModifiedDate
+        Instant lastModifiedDate,
+
+        @Version
+        int version
 ) {
+        /**
+         * Creates a new Book entity, with id = null, version = 0.
+         */
+        public static Book of(
+                String isbn, String title, String author, Double price, String publisher
+        ) {
+                return new Book(null, isbn, title, author, price, publisher, null, null, 0);
+        }
 }
